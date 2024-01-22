fd94035c6574b983064e7116a7c79c035f77b962
Hasib Ahmed <hasibahmed1054@gmail.com>
2024-01-22 19:33:37 +0600

    mobile sidebar , sheet

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -6,6 +6,7 @@ import Link from 'next/link'
 import React from 'react'
 import { Button } from './ui/button'
 import { ModeToggle } from './mode-toggle'
+import MobileSidebar from './mobile-sidebar'
 
 const font = Poppins({
     weight: "600",
@@ -16,7 +17,7 @@ const Navbar = () => {
   return (
     <div className='fixed w-full z-50 flex justify-between items-center py-2 px-4 border-b border-primary/10 bg-secondary h-16'>
         <div className='flex items-center'>
-            <Menu className='block md:hidden'/>
+            <MobileSidebar />
             <Link href='/'>
                 <h1 className={cn('hidden md:block text-xl text-3xl font-bold text-primary',
                 font.className)}>companion.ai</h1>
